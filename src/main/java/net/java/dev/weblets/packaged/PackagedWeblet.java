d7099198c835ca7a14a79d977c96a4225b2884c6
jfallows <jfallows@c71d0f0e-e102-0410-94ad-c789fbfe71d1>
2005-12-31 20:53:11 +0000

    Added Apache 2.0 license information comment.
    git-svn-id: https://weblets.dev.java.net/svn/weblets/trunk@7 c71d0f0e-e102-0410-94ad-c789fbfe71d1

--- src/main/java/net/java/dev/weblets/packaged/PackagedWeblet.java
+++ src/main/java/net/java/dev/weblets/packaged/PackagedWeblet.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2005 John R. Fallows
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.java.dev.weblets.packaged;
 
 import java.io.BufferedReader;
@@ -62,13 +77,13 @@ public class PackagedWeblet extends Weblet
         {
           BufferedReader reader = new BufferedReader(new InputStreamReader(in));
           BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
-  
+
           try
           {
             while (reader.ready())
             {
               String line = reader.readLine();
-              
+
               // TODO: tidy up pattern matching, use leading single/double quote
               // TODO: conditional filtering
               int startAt = line.indexOf("weblet:/");
@@ -83,11 +98,11 @@ public class PackagedWeblet extends Weblet
                   // TODO: fixup pattern definition
                   String pathInfo   = "/" + matcher.group(2);
                   String postamble  = matcher.group(3);
-    
+
                   // default relative weblet:/resource.ext to this weblet
                   if (webletName == null)
                     webletName = getWebletConfig().getWebletName();
-                    
+
                   WebletContainer container = WebletContainer.getInstance();
                   String webletURL = container.getWebletURL(webletName, pathInfo);
                   writer.append(preamble);
@@ -148,7 +163,7 @@ public class PackagedWeblet extends Weblet
   }
 
   private String _resourceRoot;
-  
+
   private static final Pattern _WEBLET_PROTOCOL =
                           Pattern.compile("weblet:/(?:/([^/]+)/)?([^\"\\']+)(.*)");
 }
