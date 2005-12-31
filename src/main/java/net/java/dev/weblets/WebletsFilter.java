074ce981c276e14dd58d2474b979d4870934cd6a
jfallows <jfallows@c71d0f0e-e102-0410-94ad-c789fbfe71d1>
2005-12-31 20:52:03 +0000

    Added Apache 2.0 license information comment at the top of each Java source file.
    git-svn-id: https://weblets.dev.java.net/svn/weblets/trunk@6 c71d0f0e-e102-0410-94ad-c789fbfe71d1

--- src/main/java/net/java/dev/weblets/WebletsFilter.java
+++ src/main/java/net/java/dev/weblets/WebletsFilter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2005 John R. Fallows
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.java.dev.weblets;
 
 import java.io.BufferedReader;
@@ -15,18 +30,18 @@ import javax.servlet.ServletResponse;
 
 /**
  * The WebletsFilter maps requested URLs to Weblet resources.
- * 
- * The <code>META-INF/services/net.java.dev.weblets.WebletsFilter</code> 
- * Service Provider configuration file is used to lookup the implementation 
+ *
+ * The <code>META-INF/services/net.java.dev.weblets.WebletsFilter</code>
+ * Service Provider configuration file is used to lookup the implementation
  * class for this Filter, as defined by the JAR file specification.
  */
 public class WebletsFilter implements Filter
 {
   /**
    * Initializes this Filter.
-   * 
+   *
    * @param config  the filter configuration
-   * 
+   *
    * @throws ServletException  if an error occurs
    */
   public void init(
@@ -59,14 +74,14 @@ public class WebletsFilter implements Filter
   }
 
   /**
-   * Processes the incoming request, by looking up the Weblet 
+   * Processes the incoming request, by looking up the Weblet
    * mapped to the incoming request URL pattern, and dispatching
    * to the Weblet if found, otherwise the filter chain is executed.
-   * 
+   *
    * @param request   the servlet request
    * @param response  the servlet response
    * @param chain     the filter chain
-   * 
+   *
    * @throws IOException       if an I/O error occurs
    * @throws ServletException  if an error occurs
    */
@@ -77,12 +92,12 @@ public class WebletsFilter implements Filter
   {
     _delegate.doFilter(request, response, chain);
   }
-  
+
   private Filter _delegate;
 
   // the WebletsFilter Service Provider implementation class
   static private final Class _WEBLETS_FILTER_CLASS;
-  
+
   static
   {
     try
--- src/main/java/net/java/dev/weblets/WebletsServlet.java
+++ src/main/java/net/java/dev/weblets/WebletsServlet.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2005 John R. Fallows
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.java.dev.weblets;
 
 import java.io.BufferedReader;
@@ -13,18 +28,18 @@ import javax.servlet.ServletResponse;
 
 /**
  * The WebletsServlet maps requested URLs to Weblet resources.
- * 
- * The <code>META-INF/services/net.java.dev.weblets.WebletsServlet</code> 
- * Service Provider configuration file is used to lookup the implementation 
+ *
+ * The <code>META-INF/services/net.java.dev.weblets.WebletsServlet</code>
+ * Service Provider configuration file is used to lookup the implementation
  * class for this Servlet, as defined by the JAR file specification.
  */
 public class WebletsServlet implements Servlet
 {
   /**
    * Initializes this Servlet.
-   * 
+   *
    * @param config  the servlet configuration
-   * 
+   *
    * @throws ServletException  if an error occurs
    */
   public void init(
@@ -57,7 +72,7 @@ public class WebletsServlet implements Servlet
 
   /**
    * Returns the configuration for this Servlet.
-   * 
+   *
    * @return  the configuration for this Servlet
    */
   public ServletConfig getServletConfig()
@@ -67,7 +82,7 @@ public class WebletsServlet implements Servlet
 
   /**
    * Returns the information describing this Servlet.
-   * 
+   *
    * @return  the information describing this Servlet
    */
   public String getServletInfo()
@@ -76,18 +91,18 @@ public class WebletsServlet implements Servlet
   }
 
   /**
-   * Processes the incoming request, by looking up the Weblet 
+   * Processes the incoming request, by looking up the Weblet
    * mapped to the incoming request URL pattern, and dispatching
    * to the Weblet if found, otherise sends 404 Not Found response.
-   * 
+   *
    * @param request   the servlet request
    * @param response  the servlet response
-   * 
+   *
    * @throws IOException       if an I/O error occurs
    * @throws ServletException  if an error occurs
    */
   public void service(
-    ServletRequest  request, 
+    ServletRequest  request,
     ServletResponse response) throws IOException, ServletException
   {
     _delegate.service(request, response);
@@ -97,7 +112,7 @@ public class WebletsServlet implements Servlet
 
   // the WebletsServlet Service Provider implementation class
   static private final Class _WEBLETS_SERVLET_CLASS;
-  
+
   static
   {
     try
