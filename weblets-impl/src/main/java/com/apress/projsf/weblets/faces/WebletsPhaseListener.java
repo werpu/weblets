8fddaf6561f0027dbb05a2a50d23af0f1d5bb786
jfallows <jfallows@c71d0f0e-e102-0410-94ad-c789fbfe71d1>
2006-12-02 07:26:15 +0000

    Detect re-entry call and avoid recursion.
    Re-entry can happen when running a Faces Lifecycle from within a Weblet when the Weblet itself is invoked via a PhaseListener attached to the main Faces Lifecycle.
    
    git-svn-id: https://weblets.dev.java.net/svn/weblets/trunk@83 c71d0f0e-e102-0410-94ad-c789fbfe71d1

--- weblets-impl/src/main/java/com/apress/projsf/weblets/faces/WebletsPhaseListener.java
+++ weblets-impl/src/main/java/com/apress/projsf/weblets/faces/WebletsPhaseListener.java
@@ -40,90 +40,107 @@ import com.apress.projsf.weblets.servlets.WebletResponseImpl;
 import javax.servlet.http.HttpServletRequest;
 import net.java.dev.weblets.WebletContainer;
 
-public class WebletsPhaseListener implements PhaseListener
-{
-  public void afterPhase(
-    PhaseEvent event)
-  {
-  }
-
-  public void beforePhase(
-    PhaseEvent event)
-  {
-    FacesContext context = FacesContext.getCurrentInstance();
-    ExternalContext external = context.getExternalContext();
-
-    Map applicationScope = external.getApplicationMap();
-    //WebletContainerImpl container = (WebletContainerImpl)applicationScope.get(WebletsContextListenerImpl.WEBLET_CONTAINER_KEY);
-    WebletContainerImpl container = (WebletContainerImpl)WebletContainer.getInstance();
-
-    String pathInfo = external.getRequestServletPath();
-    if (external.getRequestPathInfo() != null)
-      pathInfo += external.getRequestPathInfo();
-
-    Matcher matcher = container.getPattern().matcher(pathInfo);
-    if (matcher.matches())
-    {
-      Map requestHeaders = external.getRequestHeaderMap();
-      String contextPath = external.getRequestContextPath();
-      String requestURI = matcher.group(1);
-      String ifModifiedHeader = (String)requestHeaders.get("If-Modified-Since");
-
-      long ifModifiedSince = -1L;
-
-      if (ifModifiedHeader != null)
-      {
-        try
+public class WebletsPhaseListener implements PhaseListener {
+    
+    private static ThreadLocal reentry = 
+        new ThreadLocal()
         {
-          DateFormat rfc1123 = new HttpDateFormat();
-          Date parsed = rfc1123.parse(ifModifiedHeader);
-          ifModifiedSince = parsed.getTime();
-        }
-        catch (ParseException e)
-        {
-          throw new FacesException(e);
-        }
-      }
+            protected Object initialValue() 
+            {
+                return Boolean.FALSE;
+            }
+        };
 
-      try
-      {
-        String[] parsed =
-          container.parseWebletRequest(contextPath, requestURI, ifModifiedSince);
-
-        if (parsed != null)
-        {
-          ServletContext servletContext = (ServletContext)external.getContext();
-          HttpServletRequest httpRequest = (HttpServletRequest)external.getRequest();
-          HttpServletResponse httpResponse = (HttpServletResponse)external.getResponse();
-
-          String webletName = parsed[0];
-          String webletPath = parsed[1];
-          String webletPathInfo = parsed[2];
-          WebletRequest webRequest = 
-            new WebletRequestImpl(webletName, webletPath, contextPath, webletPathInfo, 
-                                  ifModifiedSince, httpRequest);
-          String contentName = webRequest.getPathInfo();
-          String contentTypeDefault = servletContext.getMimeType(contentName);
-          WebletResponse webResponse =
-            new WebletResponseImpl(contentTypeDefault, httpResponse);
-          container.service(webRequest, webResponse);
-          context.responseComplete();
-        }
-      }
-      catch (IOException e)
-      {
-        throw new FacesException(e);
-      }
+    public void afterPhase(
+            PhaseEvent event) {
     }
-  }
+    
+    public void beforePhase(
+            PhaseEvent event) 
+    {
+        Boolean isReentry = (Boolean)reentry.get();
 
-  public PhaseId getPhaseId()
-  {
-    return PhaseId.RESTORE_VIEW;
-  }
+        try 
+        {
+            if (isReentry == Boolean.FALSE) 
+            {
+                reentry.set(Boolean.TRUE);
+                doBeforePhase(event);
+            }
+        }
+        finally
+        {
+            reentry.set(isReentry);
+        }
+    }
 
-  /**
-   * The serialization version. 
-   */
-  private static final long serialVersionUID = -8385571916376473831L;
+    protected void doBeforePhase(
+            PhaseEvent event) 
+    {
+        FacesContext context = FacesContext.getCurrentInstance();
+        ExternalContext external = context.getExternalContext();
+
+        Map applicationScope = external.getApplicationMap();
+        //WebletContainerImpl container = (WebletContainerImpl)applicationScope.get(WebletsContextListenerImpl.WEBLET_CONTAINER_KEY);
+        WebletContainerImpl container = (WebletContainerImpl)WebletContainer.getInstance();
+
+        String pathInfo = external.getRequestServletPath();
+        if (external.getRequestPathInfo() != null)
+            pathInfo += external.getRequestPathInfo();
+
+        Matcher matcher = container.getPattern().matcher(pathInfo);
+        if (matcher.matches()) {
+            Map requestHeaders = external.getRequestHeaderMap();
+            String contextPath = external.getRequestContextPath();
+            String requestURI = matcher.group(1);
+            String ifModifiedHeader = (String)requestHeaders.get("If-Modified-Since");
+
+            long ifModifiedSince = -1L;
+
+            if (ifModifiedHeader != null) {
+                try {
+                    DateFormat rfc1123 = new HttpDateFormat();
+                    Date parsed = rfc1123.parse(ifModifiedHeader);
+                    ifModifiedSince = parsed.getTime();
+                } catch (ParseException e) {
+                    throw new FacesException(e);
+                }
+            }
+
+            try {
+                String[] parsed =
+                        container.parseWebletRequest(contextPath, requestURI, ifModifiedSince);
+
+                if (parsed != null) {
+                    ServletContext servletContext = (ServletContext)external.getContext();
+                    HttpServletRequest httpRequest = (HttpServletRequest)external.getRequest();
+                    HttpServletResponse httpResponse = (HttpServletResponse)external.getResponse();
+
+                    String webletName = parsed[0];
+                    String webletPath = parsed[1];
+                    String webletPathInfo = parsed[2];
+                    WebletRequest webRequest =
+                            new WebletRequestImpl(webletName, webletPath, contextPath, webletPathInfo,
+                            ifModifiedSince, httpRequest);
+                    String contentName = webRequest.getPathInfo();
+                    String contentTypeDefault = servletContext.getMimeType(contentName);
+                    WebletResponse webResponse =
+                            new WebletResponseImpl(contentTypeDefault, httpResponse);
+                    container.service(webRequest, webResponse);
+                    context.responseComplete();
+                }
+            } catch (IOException e) {
+                throw new FacesException(e);
+            }
+        }
+    }
+    
+    public PhaseId getPhaseId() {
+        return PhaseId.RESTORE_VIEW;
+    }
+    
+    /**
+     * The serialization version.
+     */
+    private static final long serialVersionUID = -8385571916376473831L;
 }
