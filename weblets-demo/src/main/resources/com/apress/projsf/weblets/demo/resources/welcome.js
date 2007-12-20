e89e194f5b25cbaf4d1526394986f5fedeed19ad
werpu12 <werpu12@c71d0f0e-e102-0410-94ad-c789fbfe71d1>
2007-12-20 20:39:08 +0000

    another bugfix
    git-svn-id: https://weblets.dev.java.net/svn/weblets/branches/pre_10@113 c71d0f0e-e102-0410-94ad-c789fbfe71d1

--- weblets-demo/src/main/resources/com/apress/projsf/weblets/demo/resources/welcome.js
+++ weblets-demo/src/main/resources/com/apress/projsf/weblets/demo/resources/welcome.js
@@ -2,5 +2,5 @@ function sayHello()
 {
   alert('Hello, world!');
   //rendered weblet url in a javascript
-  alert('weblet:url( com.apress.projsf.weblets.demo , welcome.js )');
+  alert('weblet:url( com.apress.projsf.weblets.demo , /welcome.js )');
 }
