bcdb8028a959a7c9c9d2e87da0ddf8b63df106a9
werpu12 <werpu12@c71d0f0e-e102-0410-94ad-c789fbfe71d1>
2008-01-09 22:43:07 +0000

    moving to trunk
    git-svn-id: https://weblets.dev.java.net/svn/weblets/branches/pre_10@154 c71d0f0e-e102-0410-94ad-c789fbfe71d1

--- weblets-demo/src/main/resources/net/java/dev/weblets/demo/resources/welcome.js
+++
@@ -1,7 +0,0 @@
-function sayHello()
-{
-  alert('Hello, world!');
-  //rendered weblet url in a javascript
-  alert('weblet:url( weblets.demo , /welcome.js )');
-  alert('weblet:resource( weblets.demo , /welcome.js )');  
-}
