ff4e183f56350b7f91b4a4744889424585c96c7c
werpu12 <werpu12@c71d0f0e-e102-0410-94ad-c789fbfe71d1>
2008-03-04 13:50:09 +0000

    added quotes to the resources
    git-svn-id: https://weblets.dev.java.net/svn/weblets/trunk@234 c71d0f0e-e102-0410-94ad-c789fbfe71d1

--- weblets-demo/src/main/resources/net/java/dev/weblets/demo/resources/welcome.js
+++ weblets-demo/src/main/resources/net/java/dev/weblets/demo/resources/welcome.js
@@ -2,6 +2,6 @@ function sayHello()
 {
   alert('Hello, world!');
   //rendered weblet url in a javascript
-  alert('weblet:url( weblets.demo , /welcome.js )');
-  alert('weblet:resource( weblets.demo , /welcome.js )');
+  alert('weblet:url( "weblets.demo" , "/welcome.js" )');
+  alert("weblet:resource( 'weblets.demo', '/welcome.js' )");
 }
